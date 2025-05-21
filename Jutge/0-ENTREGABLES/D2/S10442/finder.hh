eb08ca61e44e3e72efb311f61f1c6ad9ef5d69d3
Teo Jimenez <82720831+teojimenez@users.noreply.github.com>
2025-05-21 19:19:24 +0200

    Add files via upload

--- Jutge/0-ENTREGABLES/D2/S10442/finder.hh
+++ Jutge/0-ENTREGABLES/D2/S10442/finder.hh
@@ -14,17 +14,19 @@
 template <class T>
 class Finder {
     private:
-
+        static constexpr int CELL = 256;
         // relacio obj(individual) amb les seves coordenades
 
         //ens serveix per tenir juntes les coordenades d'un sol obj
 
         //MIRAR SI NO FA FALTA IGUAL NO
+        //realment ens serveix nomes per borrar un item
         std::map<const T*, std::set<std::pair<int, int>>> individual_items_coords_;
 
         // relacio coordenades amb conjunts d'objs que estan allà
 
         //ens serveix per tenir una graella de coordenades amb tots els objectes
+        //aqui tot en / CELL
         std::map<std::pair<int, int>, std::set<const T*>>    coords_map_;
 
 
@@ -43,9 +45,9 @@ class Finder {
         void add(const T *t)
         {
             auto rect = t->get_rect();
-            for (int x = rect.left; x <= rect.right; x++)
+            for (int x = rect.left / CELL; x <= rect.right / CELL; x++)
             {
-                for (int y = rect.top; y <= rect.bottom; y++)
+                for (int y = rect.top / CELL; y <= rect.bottom / CELL; y++)
                 {
                     individual_items_coords_[t].insert({x, y});
                     coords_map_[{x, y}].insert(t);
@@ -82,25 +84,68 @@ class Finder {
         {
             set <const T *> result;
 
-            for (int x = qrect.left; x <= qrect.right; x++)
+            // for (int x = qrect.left / CELL; x <= qrect.right / CELL; x++)
+            // {
+            //     for (int y = qrect.top / CELL; y <= qrect.bottom / CELL; y++)
+            //     {
+            //         auto it = coords_map_.find({x, y});
+            //         if (it != coords_map_.end())//existeix
+            //         {
+            //             for (const T *t : it->second )
+            //             {
+            //                 // auto it_obj = result.find(t);
+            //                 //mirar de no afegir repetits(no importa pero sobra)
+            //                 pro2::Rect a = {qrect.left / CELL, qrect.top / CELL, qrect.right / CELL, qrect.bottom / CELL};
+            //                 if (obj_inside(a, t->get_rect()) ||  (result.size() == 0) )
+            //                 {
+            //                     result.insert(t);
+            //                 }
+            //             }
+            
+            //         }
+            //     }
+            // }
+        
+        int cell_left = qrect.left   / CELL;
+        int cell_right = qrect.right  / CELL;
+        int cell_top = qrect.top    / CELL;
+        int cell_bottom = qrect.bottom / CELL;
+
+        for (int x = cell_left; x <= cell_right; ++x)
+        {
+            //busquem de left a right, i top a bottom: si a cada coordenada (/ CELL) hi ha algun punt d'un obj
+            for (int y = cell_top; y <= cell_bottom; ++y)
             {
-                for (int y = qrect.top; y <= qrect.bottom; y++)
+                auto it = coords_map_.find({x, y});
+                //busqueda en coords_map en / 256
+                if (it != coords_map_.end())
                 {
-                    auto it = coords_map_.find({x, y});
-                    if (it != coords_map_.end())//existeix
+                    //busquem per cada coordenada
+                    //tots els punts i mirem si estan dins
+                    for (const T* obj : it->second)
                     {
-                        for (const T *t : it->second )
-                        {
-                            // auto it_obj = result.find(t);
-                            if (obj_inside(qrect, t->get_rect()) )
-                            {
-                                result.insert(t);
-                            }
-                        }
-                        
+                        //mirem amb les dades reals, si el obj esta dins o no
+                        if (obj_inside(qrect, obj->get_rect()))
+                            result.insert(obj);
                     }
                 }
             }
+        }
+            //&  referia no, copia
+            // for (const auto& [coord, objects] : coords_map_)
+            // {
+            //     pro2::Rect a = {qrect.left / CELL, qrect.top / CELL, qrect.right / CELL, qrect.bottom / CELL};
+            //     if (obj_inside(a, pro2::Rect{coord.first,coord.second,coord.first,coord.second}))
+            //     {
+            //         for (const T *t : objects )
+            //         {
+            //             result.insert(t);
+            //         }
+                    
+
+            //     }
+            // }
+            
             return result;
         }
 };
